61d9c1e1375656e22c35b21a3385ebc782e6908b
akshaykkgit <akshaykk3@gmail.com>
2022-02-12 21:38:29 -0800

    oauth 2.0 has been implemented

--- cloud-gateway/src/main/java/com/akshay/cloudgateway/config/GatewayConfig.java
+++ cloud-gateway/src/main/java/com/akshay/cloudgateway/config/GatewayConfig.java
@@ -6,18 +6,29 @@ import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
 
 @Configuration
 public class GatewayConfig {
-   /* @Bean
-    public Customizer<Resilience4JCircuitBreakerFactory> slowCustomizer() {
-        return factory -> factory.configure(builder -> builder.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
-                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(6)).build()), "slow");
+    /*@Bean
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build())
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .build());
     }*/
+
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder loadbalancedWebClientBuilder(){
+        return WebClient.builder();
+    }
+    
 
 }
